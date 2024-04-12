13cf34cc8b6028f33ce92446e43e6dffe73f0662
isabel <isabel@isabelroses.com>
2024-04-12 15:43:04 +0100

    feat: fullscreen support

--- cmd/mainModal.go
+++ cmd/mainModal.go
@@ -6,32 +6,51 @@ import (
 	"strconv"
 
 	"github.com/charmbracelet/bubbles/table"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 var feeds = GetAllContent()
 
 type model struct {
-	context string
-	feeds   Feeds
-	posts   Posts
-	table   table.Model
+	context  string
+	feeds    Feeds
+	posts    Posts
+	viewport viewport.Model
+	table    table.Model
+	ready    bool
 }
 
 func (m model) Init() tea.Cmd { return nil }
 
-func (m model) View() string { return m.table.View() }
+func (m model) View() string {
+	if !m.ready {
+		return "\n  Initializing..."
+	}
+	return m.viewport.View()
+}
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
 
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		if !m.ready {
+			m.viewport = viewport.New(msg.Width, msg.Height)
+			m.ready = true
+		} else {
+			m.viewport.Width = msg.Width
+			m.viewport.Height = msg.Height
+		}
+
 	case tea.KeyMsg:
 		switch msg.String() {
-
 		case "esc", "q", "ctrl+c":
 			if m.context == "content" {
-				return loadHome(m), nil
+				m = loadHome(m)
 			} else {
 				return m, tea.Quit
 			}
@@ -43,14 +62,21 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				return m, nil
 			} else {
 				id, _ := strconv.Atoi(m.table.SelectedRow()[0])
-				return loadContent(m, m.feeds[id]), nil
+				m = loadContent(m, m.feeds[id])
 			}
 		}
 	}
+
 	m.table, cmd = m.table.Update(msg)
-	return m, cmd
+	cmds = append(cmds, cmd)
+	m.viewport.SetContent(m.table.View())
+	m.viewport, cmd = m.viewport.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
 }
 
+// load the home view, this conists of the list of feeds
 func loadHome(m model) model {
 	columns := []table.Column{
 		{Title: "ID", Width: 2},
@@ -80,6 +106,7 @@ func loadHome(m model) model {
 
 	m.context = "urls"
 	m.table = t
+	m.ready = true
 
 	return m
 }
@@ -132,7 +159,7 @@ func loadReader(post Post) {
 }
 
 func Run() {
-	m := model{"urls", feeds, Posts{}, table.Model{}}
+	m := model{"urls", feeds, Posts{}, viewport.Model{}, table.Model{}, false}
 	m = loadHome(m)
 
 	if _, err := tea.NewProgram(m).Run(); err != nil {
