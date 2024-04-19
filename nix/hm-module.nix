602bdee2400e7c33490aa5f078285fb8494b0d7b
isabel <isabel@isabelroses.com>
2024-04-19 23:37:29 +0100

    feat(nix): include config.toml settings

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -15,6 +15,55 @@ with lib; {
       example = ["http://example.com"];
       description = "Feed URLs.";
     };
+
+    settings = {
+      dateformat = mkOption {
+        type = types.str;
+        example = "02/01/2006";
+        default = "";
+        description = ''
+          The date format show for when a post was published
+          see <https://go.dev/src/time/format.go> for more information
+        '';
+      };
+
+      colors = {
+        text = mkOption {
+          type = types.str;
+          example = "#cdd6f4";
+          default = "";
+          description = "Text color";
+        };
+
+        subtext = mkOption {
+          type = types.str;
+          example = "#a6adc8";
+          default = "";
+          description = "Subtext color";
+        };
+
+        inverttext = mkOption {
+          type = types.str;
+          example = "#1e1e2e";
+          default = "";
+          description = "Inverted text color";
+        };
+
+        accent = mkOption {
+          type = types.str;
+          example = "#74c7ec";
+          default = "";
+          description = "Accent color";
+        };
+
+        borders = mkOption {
+          type = types.str;
+          example = "#313244";
+          default = "";
+          description = "Borders color";
+        };
+      };
+    };
   };
 
   config = let
@@ -23,6 +72,16 @@ with lib; {
     mkIf cfg.enable {
       home.packages = [self.packages.${pkgs.stdenv.hostPlatform.system}.default];
 
-      xdg.configFile."izrss/urls" = mkIf (cfg.urls != []) {text = concatStringsSep "\n" cfg.urls;};
+      xdg.configFile = {
+        "izrss/urls" = mkIf (cfg.urls != []) {text = concatStringsSep "\n" cfg.urls;};
+        "izrss/config.toml".source = (pkgs.formats.toml {}).generate "config.toml" cfg.settings;
+      };
+
+      assertions = [
+        {
+          assertion = config.xdg.enable;
+          message = "Option xdg.enable must be enabled for the configuration to be written to the filesystem.";
+        }
+      ];
     };
 }
