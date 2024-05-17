ce708d8d34ff7bb49a104434d77f4b5ec1c20dcb
isabel <isabel@isabelroses.com>
2024-05-17 19:45:07 +0100

    chore: cleanup hm module

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -1,62 +1,67 @@
-self: {
-  config,
+self:
+{
   lib,
   pkgs,
+  config,
   ...
-}: let
-  inherit (lib) mkIf types mkOption mkEnableOption mkPackageOption concatStringsSep;
+}:
+let
+  inherit (lib)
+    mkIf
+    types
+    mkOption
+    mkEnableOption
+    mkPackageOption
+    concatStringsSep
+    ;
 
-  mkColorOption = name: color:
-    mkOption {
-      type = types.str;
-      example = color;
-      default = "";
-      description = "${name} color";
-    };
-in {
-  meta.maintainers = [lib.maintainers.isabelroses];
+  settingsFormat = pkgs.formats.toml { };
+in
+{
+  meta.maintainers = [ lib.maintainers.isabelroses ];
 
   options.programs.izrss = {
     enable = mkEnableOption "A fast and once simple cli todo tool";
 
-    package = mkPackageOption self.packages.${pkgs.stdenv.hostPlatform.system} "izrss" {};
+    package = mkPackageOption self.packages.${pkgs.stdenv.hostPlatform.system} "izrss" { };
 
     urls = mkOption {
       type = with types; listOf str;
-      example = ["http://example.com"];
+      example = [ "http://example.com" ];
       description = "Feed URLs.";
     };
 
-    settings = {
-      dateformat = mkOption {
-        type = types.str;
-        example = "02/01/2006";
-        default = "";
-        description = ''
-          The date format show for when a post was published
-          see <https://go.dev/src/time/format.go> for more information
-        '';
-      };
+    settings = mkOption {
+      inherit (settingsFormat) type;
+      default = { };
+      example = lib.literalExpression ''
+        dateformat = "02/01/2006"
 
-      colors = {
-        text = mkColorOption "Text" "#cdd6f4";
-        subtext = mkColorOption "Subtext" "#a6adc8";
-        inverttext = mkColorOption "Invert text" "#1e1e2e";
-        accent = mkColorOption "Accent" "#74c7ec";
-        borders = mkColorOption "Borders" "#313244";
-      };
+        [colors]
+        text = "#cdd6f4"
+        inverttext = "#1e1e2e"
+        subtext = "#a6adc8"
+        accent = "#74c7ec"
+        borders = "#313244"
+      '';
+      description = ''
+        Configuration written to {file}`$XDG_CONFIG_HOME/izrss/config.toml`.
+
+        See <https://github.com/isabelroses/izrss/blob/main/example.toml> for the documentation.
+      '';
     };
   };
 
-  config = let
-    cfg = config.programs.izrss;
-  in
+  config =
+    let
+      cfg = config.programs.izrss;
+    in
     mkIf cfg.enable {
-      home.packages = [cfg.package];
+      home.packages = [ cfg.package ];
 
       xdg.configFile = {
-        "izrss/urls" = mkIf (cfg.urls != []) {text = concatStringsSep "\n" cfg.urls;};
-        "izrss/config.toml".source = (pkgs.formats.toml {}).generate "config.toml" cfg.settings;
+        "izrss/urls" = mkIf (cfg.urls != [ ]) { text = concatStringsSep "\n" cfg.urls; };
+        "izrss/config.toml".source = settingsFormat.generate "izrss-config.toml" cfg.settings;
       };
 
       assertions = [
